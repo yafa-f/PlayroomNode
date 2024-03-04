08f089e2f6cd9fd90152c95d6174a55e294cb9ef
yafa <yafa0694@gmail.com>
2024-03-04 10:55:48 +0200

    models

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const app = express();
+const myExpress = require('express');
+const app = myExpress();
 const port = 3000;
-
+const cors=require("cors");
+const bodyParser=require("body-parser");
+app.use(bodyParser());
+app.use(cors({
+    origin:'*'
+}));
 app.get('/', (req, res) => {
   res.send('Hello, Node.js!');
 });
